2a50dec0ec3cfd7e8b4acac8a3dbb13f49d29487
Jason Yip <jasonyip1234567@gmail.com>
2021-09-28 09:39:31 -0500

    [ADD]: Custom output destination

--- musicnotes_rm/MusicnotesRM.py
+++ musicnotes_rm/MusicnotesRM.py
@@ -1,19 +1,25 @@
 from pathlib import Path
 import fitz
 import io
-from typing import Union
+from typing import Union, Optional
 import re
 import time
 from pprint import pp
 
-def rm_watermark(pdf: Path, name: str) -> Path:
+def rm_watermark(pdf: Path, name: str, output: Optional[Path]) -> Path:
     doc = fitz.open(pdf)
     for page in doc:
         rects = page.search_for(name)
         for rect in rects:
             page.add_redact_annot(rect)
         page.apply_redactions()
-    par_folder = pdf.parents[1] / "new"
-    if not par_folder.is_dir():
-        par_folder.mkdir()
-    doc.save((par_folder / (time.strftime("%Y%m%d-%H%M%S") + ' ' + pdf.stem)).with_suffix(".pdf"))
+    if output is None:
+        if pdf.parent.name == "old":
+            par_folder = pdf.parents[1] / "new"
+            if not par_folder.is_dir():
+                par_folder.mkdir()
+        else:
+            par_folder = pdf.parent
+        doc.save((par_folder / (time.strftime("%Y%m%d-%H%M%S") + ' ' + pdf.stem)).with_suffix(".pdf"))
+    else:
+        doc.save(output)
--- musicnotes_rm/__init__.py
+++ musicnotes_rm/__init__.py
@@ -9,6 +9,7 @@ def _main(args = None):
     parser = argparse.ArgumentParser()
 
     parser.add_argument("pdf", type=Path)
+    parser.add_argument("-o", "--output", type=Path, default=None)
     parser.add_argument("name", nargs='+')
 
     args = parser.parse_args(args)
@@ -16,7 +17,7 @@ def _main(args = None):
     if not (args.pdf.suffix == ".pdf" and args.pdf.is_file()):
         parser.error("Invalid PDF file")
 
-    MusicnotesRM.rm_watermark(args.pdf, ' '.join(args.name))
+    MusicnotesRM.rm_watermark(args.pdf, ' '.join(args.name), args.output)
 
 if __name__ == "__main__":
     _main()
